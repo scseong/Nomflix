2dc0d3fe025b825608f792e0b1b5a4f58389ee41
scseong <goidtv@naver.com>
2023-01-16 17:56:21 +0900

    feat: 상단 헤더 구현 Netflix 상단 헤더 구현 로고 애니메이션  적용 Navigation 현재 위치 표시 검색창 애니메이션 적용 페이지 스크롤할 때 헤더 투명도 조절

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import App from './App';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Tv from './pages/Tv';
+import Root from './Root';
 
 const Router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'tv',
+        element: <Tv />,
+      },
+      {
+        path: 'search',
+        element: <Search />,
+      },
+    ],
   },
 ]);
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,19 @@ import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import { myTheme } from './theme';
+import GlobalStyles from './GlobalStyles';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={myTheme}>
       <RecoilRoot>
+        <GlobalStyles />
         <RouterProvider router={Router} />
       </RecoilRoot>
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
